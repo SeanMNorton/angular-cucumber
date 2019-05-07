df44a42a49f51937d98f79df2a2549d3db6ba7ba
Sean <nortonseanm@gmail.com>
2019-05-07 13:43:58 -0400

    setup and add protractor cucumber

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,10 +2,14 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get('/');
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  getHeader() {
+    return element(by.css('app-root h1')).getText();
+  }
+
+  getTitle() {
+    return element(by.css('form'));
   }
 }
---
+++ e2e/src/tests/myfeature.steps.ts
@@ -0,0 +1,33 @@
+import { Given, Before, When, Then } from 'cucumber';
+import { browser, element, by, ExpectedConditions } from 'protractor';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import { AppPage } from '../app.po';
+const expect = chai.use(chaiAsPromised).expect;
+
+let appPage: AppPage = new AppPage();
+browser.waitForAngularEnabled(false);
+
+Before(() => {
+  appPage = new AppPage();
+});
+
+Given('I open app page', () => {
+  appPage.navigateTo();
+});
+
+When('app page loads', () => {
+  // console.log( browser.ExpectedConditions.visibilityOf(appPage.getTitle()) );
+  return ExpectedConditions.visibilityOf(appPage.getTitle());
+  // return browser.wait(
+  //   ExpectedConditions.visibilityOf(appPage.getTitle()
+  // ), 5000);
+});
+
+// title passed in from feature file
+Then('header {string} is displayed', (title) => {
+  return appPage.getHeader()
+    .then(text => {
+      expect(text).to.have.string(title);
+    });
+});
