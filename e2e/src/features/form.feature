60a6a52a15f8a8d8630f01c5c3530522d155c4b9
Jonathan Norstrom <jon.norstrom@gmail.com>
2019-05-07 15:57:31 -0400

    tested todo app

---
+++ e2e/src/features/form.feature
@@ -0,0 +1,6 @@
+Feature: Form
+
+  Scenario: Seeing the Form
+    Given I open the app
+    When The app page loads
+    Then Todo form is displayed
---
+++ e2e/src/features/form.steps.ts
@@ -0,0 +1,33 @@
+import { AppPage } from '../app.po'
+import { Given, When, Then } from 'cucumber';
+import {
+  ExpectedConditions as EC,
+  browser,
+  element,
+  by
+} from 'protractor'
+
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+const expect = chai.use(chaiAsPromised).expect
+
+
+
+let appPage: AppPage = new AppPage();
+browser.waitForAngularEnabled(false);
+
+Given('I open the app', () => {
+  appPage.navigateTo();
+})
+
+When('The app page loads', () => {
+  browser.wait(EC.visibilityOf(element(by.tagName('title'))),5000)
+})
+
+Then('Todo form is displayed', () => {
+  // let test = EC.visibilityOf(element(by.tagName('form')))
+  return element(by.css('form#superForm')).isDisplayed().then(res => {
+    expect(res).to.be.true
+  })
+})
